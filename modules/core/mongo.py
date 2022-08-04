fe842a52d5edbf53d6f5dd52e0508c898e24718f
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-04 19:40:51 +0530

    Update mongo.py

--- modules/core/mongo.py
+++ modules/core/mongo.py
@@ -12,13 +12,13 @@ from ..logging import LOGGER
 TEMP_MONGODB = "mongodb+srv://Mrperfect1234:mrperfect2222@mrperfect.u31bh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
-if config.MONGO_DB_URI is None:
+if modules.config.MONGO_DB_URI is None:
     LOGGER(__name__).warning(
         "No MONGO DB URL found.. Your Bot will work on Telugu Coders Database"
     )
     temp_client = Client(
         "Telugucoders",
-        bot_token=config.BOT_TOKEN,
+        bot_token=modules.config.BOT_TOKEN,
         api_id=config.API_ID,
         api_hash=config.API_HASH,
     )
@@ -31,7 +31,7 @@ if config.MONGO_DB_URI is None:
     mongodb = _mongo_async_[username]
     pymongodb = _mongo_sync_[username]
 else:
-    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
-    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    _mongo_async_ = _mongo_client_(modules.config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(modules.config.MONGO_DB_URI)
     mongodb = _mongo_async_.Telugucoders
     pymongodb = _mongo_sync_.Telugucoders
