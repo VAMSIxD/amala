5153a6629c910da3fd1cf7ff4992786c05a0470a
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-04 18:23:54 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -7,6 +7,8 @@ load_dotenv()
 que = {}
 admins = {}
 
+#------------------------ Important Stuff 😌 -----------------------
+
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -15,8 +17,18 @@ STRING_SESSION = getenv("STRING_SESSION")
 BOT_USERNAME = getenv("BOT_USERNAME")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5589457533 5564501117").split()))
+
+#________________________ Updates 🍃 _______________________________
+
 NETWORK = getenv("NETWORK")
 GROUP = getenv("GROUP")
+
+##•••••••••••••••••••••••• Mongodb Url Stuff •••••••••••••••••••••••
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
+#************************* Image Stuff 💕 **************************
+
 IMG_1 = getenv("IMG_1", "https://telegra.ph/file/d6f92c979ad96b2031cba.png")
 IMG_2 = getenv("IMG_2", "https://telegra.ph/file/6213d2673486beca02967.png")
 IMG_5 = getenv("IMG_5", "https://telegra.ph/file/d08d6474628be7571f013.png") 
