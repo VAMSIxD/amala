a1b9ea39a2878b13d231f1d23f1c29593d4e0a5d
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-04 19:19:10 +0530

    Update misc.py

--- modules/misc.py
+++ modules/misc.py
@@ -1,1 +1,37 @@
+import socket
+import time
+
+from pyrogram import filters
+
+from modules.config import PRIVATE_BOT_MODE, OWNER_ID
+from modules.core.mongo import pymongodb
+
+from .logging import LOGGER
+
+SUDOERS = filters.user()
+
+
+def sudo():
+    global SUDOERS
+    OWNER = config.OWNER_ID
+    if config.MONGO_DB_URI is None:
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+    else:
+        sudoersdb = pymongodb.sudoers
+        sudoers = sudoersdb.find_one({"sudo": "sudo"})
+        sudoers = [] if not sudoers else sudoers["sudoers"]
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+            if user_id not in sudoers:
+                sudoers.append(user_id)
+                sudoersdb.update_one(
+                    {"sudo": "sudo"},
+                    {"$set": {"sudoers": sudoers}},
+                    upsert=True,
+                )
+        if sudoers:
+            for x in sudoers:
+                SUDOERS.add(x)
+    LOGGER(__name__).info(f"Sudoers Loaded.")
 
