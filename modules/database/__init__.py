8f596fe324067ca0d49a7aa5da18c3d62e78744d
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-05 20:45:11 +0530

    Update __init__.py

--- modules/database/__init__.py
+++ modules/database/__init__.py
@@ -1,4 +1,1 @@
-""" database copyright """
 
-# thanks to TheHamkerCat (https://github.com/TheHamkerCat) for his WilliamButcherBot database.
-# implemented by SR The Programmer (https://github.com/SRTheProgrammer)
