322c25e4b3145e850318f198eeb1d87f7c6883d7
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-05 13:24:33 +0530

    Create dbchat.py

---
+++ modules/database/dbchat.py
@@ -0,0 +1,17 @@
+""" chat database """
+
+from typing import Dict, List, Union
+
+from modules.database.dblocal import db
+
+chatsdb = db.chats
+
+
+async def get_served_chats() -> list:
+    chats = chatsdb.find({"chat_id": {"$lt": 0}})
+    if not chats:
+        return []
+    chats_list = []
+    for chat in await chats.to_list(length=1000000000):
+        chats_list.append(chat)
+    return chats_list
