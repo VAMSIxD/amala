e9a373eb0ac491d9226ca38faffb49573be6d20f
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-06 16:54:32 +0530

    Update clientbot.py

--- modules/clientbot/clientbot.py
+++ modules/clientbot/clientbot.py
@@ -1,8 +1,10 @@
+import os
 from pyrogram import idle
 from pyrogram import Client as Bot
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION
 from pytgcalls import PyTgCalls
+from telethon import TelegramClient
    
 bot = Bot(
     ":telugucoders:",
@@ -17,6 +19,12 @@ user = Client(
     api_id=API_ID,
     api_hash=API_HASH,
 )
+
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("TOKEN")
+client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
+spam_chats []
 
 call_py = PyTgCalls(user, overload_quiet_mode=True) 
 with Client(":telugucoders:", API_ID, API_HASH, bot_token=BOT_TOKEN) as app:
