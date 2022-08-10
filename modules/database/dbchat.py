803cbbc7962779046d9e5627ae7bba12704503a8
TeluguCodersMusic <110549203+TeluguCodersMusic@users.noreply.github.com>
2022-08-10 18:13:06 +0530

    Update dbchat.py

--- modules/database/dbchat.py
+++ modules/database/dbchat.py
@@ -5,7 +5,6 @@ from typing import Dict, List, Union
 from modules.database import db
 
 chatsdb = db.chats
-blacklist_chatdb = db.blacklistChat
 
 async def is_served_chat(chat_id: int) -> bool:
     chat = await chatsdb.find_one({"chat_id": chat_id})
@@ -36,22 +35,3 @@ async def remove_served_chat(chat_id: int):
     if not is_served:
         return
     return await chatsdb.delete_one({"chat_id": chat_id})
-
-
-async def blacklisted_chats() -> list:
-    chats = blacklist_chatdb.find({"chat_id": {"$lt": 0}})
-    return [chat["chat_id"] for chat in await chats.to_list(length=1000000000)]
-
-
-async def blacklist_chat(chat_id: int) -> bool:
-    if not await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.insert_one({"chat_id": chat_id})
-        return True
-    return False
-
-
-async def whitelist_chat(chat_id: int) -> bool:
-    if await blacklist_chatdb.find_one({"chat_id": chat_id}):
-        await blacklist_chatdb.delete_one({"chat_id": chat_id})
-        return True
-    return False
